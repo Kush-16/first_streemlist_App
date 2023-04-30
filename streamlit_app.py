25cb06ef258db0ec6ca8fca62c612acb1e3b9ff2
Kush-16 <50994580+Kush-16@users.noreply.github.com>
2023-04-30 14:55:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,13 @@ import streamlit
 streamlit.title('My parents new healthy dinner!')
 
 streamlit.header('Breakfast menu')
-streamlit.text('granlola and yogurt \n hot chocolate \n eggs and potatoes')
+streamlit.text('\n granlola and yogurt \n hot chocolate \n eggs and potatoes')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast ')
+
+
+
 
