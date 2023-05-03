63aafa6b2322284604eec03eb3e340bcd7aa710b
Kush-16 <50994580+Kush-16@users.noreply.github.com>
2023-05-03 16:08:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,5 +54,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 # streamlit.text("Hello from Snowflake:")
-streamlit.text("The fruitload list contains:")
+# streamlit.text("The fruitload list contains:")
+
+streamlit.header("The fruitload list contains:")
 streamlit.text(my_data_row)
