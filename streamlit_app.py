764fb9bd40f079101c6d7f276319bc066beceaae
Kush-16 <50994580+Kush-16@users.noreply.github.com>
2023-05-03 16:32:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,8 @@ try:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         streamlit.dataframe(fruityvice_normalized)
-        
+except URLError as e:
+    streamlit.error()
 
       
     # streamlit.write('The user entered ', fruit_choice)
@@ -59,8 +60,7 @@ try:
     # streamlit.dataframe(fruityvice_normalized)
     
     
-except URLError as e:
-    streamlit.error()
+
     
 # do not run anything beyond this
 streamlit.stop()
