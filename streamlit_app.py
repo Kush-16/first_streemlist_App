dcf9767524360b0ee23be4700b6eaa948964a00e
Kush-16 <50994580+Kush-16@users.noreply.github.com>
2023-05-03 14:41:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,12 +31,14 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice fruit advice')
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # streamlit.text(fruityvice_response.json())
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 # normalizing json to table
