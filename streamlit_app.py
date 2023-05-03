c0e0655cb540c5e0b8e13e6507b6eced29516e2c
Kush-16 <50994580+Kush-16@users.noreply.github.com>
2023-05-03 14:35:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,4 @@ streamlit.header('Fruityvice fruit advice')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
