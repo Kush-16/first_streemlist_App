fd8bd4799a1a806bb761b270a52f5af4a2866c78
Kush-16 <50994580+Kush-16@users.noreply.github.com>
2023-05-03 14:37:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ streamlit.header('Fruityvice fruit advice')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 
 
 # normalizing json to table
